3ba02c678c73aaa4f14660ff1bed669c8a8eaf2f
wafflejuice <wafflejuice96@gmail.com>
2022-07-31 22:13:42 +0900

    save page

--- src/main/kotlin/org/wafflejuice/springboot/web/IndexController.kt
+++ src/main/kotlin/org/wafflejuice/springboot/web/IndexController.kt
@@ -1,10 +1,17 @@
 package org.wafflejuice.springboot.web
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
+@Controller
 class IndexController {
     @GetMapping("/")
     fun index(): String {
         return "index"
+    }
+
+    @GetMapping("/posts/save")
+    fun postsSave(): String {
+        return "posts-save"
     }
 }
