2779305a591353771c88bfd125dcf8431ce7ec2b
wafflejuice <wafflejuice96@gmail.com>
2022-08-02 23:11:00 +0900

    update post

--- src/main/kotlin/org/wafflejuice/springboot/web/IndexController.kt
+++ src/main/kotlin/org/wafflejuice/springboot/web/IndexController.kt
@@ -3,6 +3,7 @@ package org.wafflejuice.springboot.web
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.wafflejuice.springboot.domain.posts.PostsRepository
 import org.wafflejuice.springboot.service.posts.PostsService
 
@@ -19,5 +20,16 @@ class IndexController(
     @GetMapping("/posts/save")
     fun postsSave(): String {
         return "posts-save"
+    }
+
+    @GetMapping("/posts/update/{id}")
+    fun postsUpdate(
+        @PathVariable id: Long,
+        model: Model
+    ): String {
+        val dto = postsService.findById(id)
+        model.addAttribute("post", dto)
+
+        return "posts-update"
     }
 }
