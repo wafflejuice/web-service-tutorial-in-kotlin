6e1b79a934a5ad797e825e29bea723ea9fc6d30e
wafflejuice <wafflejuice96@gmail.com>
2022-08-09 23:22:28 +0900

    resolve an error: - Mock User

--- src/test/kotlin/org/wafflejuice/springboot/web/PostsApiControllerTest.kt
+++ src/test/kotlin/org/wafflejuice/springboot/web/PostsApiControllerTest.kt
@@ -1,7 +1,9 @@
 package org.wafflejuice.springboot.web
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,11 +11,16 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 import org.wafflejuice.springboot.domain.posts.Posts
 import org.wafflejuice.springboot.domain.posts.PostsRepository
 import org.wafflejuice.springboot.web.dto.PostsSaveRequestDto
@@ -30,12 +37,26 @@ internal class PostsApiControllerTest(
     private val restTemplate: TestRestTemplate,
 
     @Autowired
-    private val postsRepository: PostsRepository
+    private val postsRepository: PostsRepository,
+
+    @Autowired
+    val context: WebApplicationContext,
 ) {
+    lateinit var mvc: MockMvc
+
+    @BeforeEach
+    fun setup() {
+        mvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply { SecurityMockMvcConfigurers.springSecurity() }
+            .build()
+    }
+
     @AfterEach
     fun tearDown() = postsRepository.deleteAll()
 
     @Test
+    @WithMockUser(roles = ["USER"])
     fun `Posts 등록`() {
         //given
         val title = "title"
@@ -46,19 +67,29 @@ internal class PostsApiControllerTest(
         val url = "http://localhost:${port}/api/v1/posts"
 
         //when
-        val responseEntity: ResponseEntity<Long> = restTemplate.postForEntity(url, requestDto, Long::javaClass)
+//        val responseEntity: ResponseEntity<Long> = restTemplate.postForEntity(url, requestDto, Long::javaClass)
+        mvc.perform(
+            post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(ObjectMapper().writeValueAsString(requestDto))
+        ).andExpect(status().isOk)
 
         //then
-        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(responseEntity.body ?: throw Exception("Reponse with null body")).isGreaterThan(0L)
-
+//        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+//        assertThat(responseEntity.body ?: throw Exception("Reponse with null body")).isGreaterThan(0L)
+//
+//        val all = postsRepository.findAll()
+//        assertThat(all[0].title).isEqualTo(title)
+//        assertThat(all[0].content).isEqualTo(content)
+//        assertThat(all[0].author).isEqualTo(author)
         val all = postsRepository.findAll()
-        assertThat(all[0].title).isEqualTo(title)
-        assertThat(all[0].content).isEqualTo(content)
-        assertThat(all[0].author).isEqualTo(author)
+        assertThat(all[0].title == title)
+        assertThat(all[0].content == content)
+        assertThat(all[0].author == author)
     }
 
     @Test
+    @WithMockUser(roles = ["USER"])
     fun `Posts 수정`() {
         //given
         val savedPosts = postsRepository.save(Posts(title = "title", content = "content", author = "author"))
@@ -70,18 +101,26 @@ internal class PostsApiControllerTest(
         val requestDto = PostsUpdateRequestDto(title = expectedTitle, content = expectedContent)
         val url = "http://localhost:${port}/api/v1/posts/${updateId}"
 
-        val requestEntity = HttpEntity<PostsUpdateRequestDto>(requestDto)
+//        val requestEntity = HttpEntity<PostsUpdateRequestDto>(requestDto)
 
         //when
-        val responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long::class.java)
+//        val responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long::class.java)
+        mvc.perform(
+            put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(ObjectMapper().writeValueAsString(requestDto))
+        ).andExpect(status().isOk)
 
         //then
-        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(responseEntity.body ?: throw Exception("Reponse with null body")).isGreaterThan(0L)
-
+//        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+//        assertThat(responseEntity.body ?: throw Exception("Reponse with null body")).isGreaterThan(0L)
+//
+//        val all = postsRepository.findAll()
+//        assertThat(all[0].title).isEqualTo(expectedTitle)
+//        assertThat(all[0].content).isEqualTo(expectedContent)
         val all = postsRepository.findAll()
-        assertThat(all[0].title).isEqualTo(expectedTitle)
-        assertThat(all[0].content).isEqualTo(expectedContent)
+        assertThat(all[0].title == expectedTitle)
+        assertThat(all[0].content == expectedContent)
     }
 
     @Test
